7b5dc3320f2df62c91898ddba083f1452a423a6d
Zane Bennion <zanebennion@gmail.com>
2026-04-29 16:37:26 -0600

    auto age calculator

--- app/components/Name.vue
+++ app/components/Name.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="container">
-    Name, age, email
+    <div class="name">Zane Bennion</div>
+    <div class="age">{{ age }} Years Old</div>
+    <div class="contact">Contact Me: 
+      <a href="mailto:zanebennion@gmail.com">zanebennion@gmail.com</a>
+    </div>
   </div>
 </template>
 
 <style lang="css" scoped>
 .container {
-  background-color: aliceblue;
   width: 100%;
   height: 100%;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.name {
+  color: pink;
 }
 </style>
+
+<script setup lang="ts">
+  const today = new Date();
+  const currYear = today.getFullYear();
+  const birthYear = 2004;
+  const age = currYear - birthYear;
+</script>
