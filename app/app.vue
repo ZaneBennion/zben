45cf7a288b96dec6f9d75a581e757a9c10600935
Zane Bennion <zanebennion@gmail.com>
2026-04-29 12:45:57 -0600

    added background

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="background">
+    Hello :3
   </div>
 </template>
+
+<style lang="css">
+body {
+  margin: 0%;
+}
+
+.background {
+  width: 100vw;
+  height: 100vh;
+  background-image: url("/img/bliss.webp");
+  background-size: cover;
+  background-position: center;
+}
+</style>
