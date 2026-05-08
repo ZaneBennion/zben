3f74286c4ca63ee709c9ca75cfa8a99f09552aea
zanebennion <zanebennion@gmail.com>
2026-05-08 10:55:03 -0600

    mail page basic setup

--- app/pages/mail.vue
+++ app/pages/mail.vue
@@ -1,3 +1,27 @@
 <template>
-    <div class="background">Mail</div>
+    <div class="background">
+        <div class="container">
+            <div class="window">
+                <div class="title-bar">
+                    <div class="title-bar-text">Inbox - Microsoft Outlook</div>
+                    <div class="title-bar-controls">
+                        <button aria-label="Minimize"></button>
+                        <button aria-label="Maximize"></button>
+                        <button aria-label="Close"></button>
+                    </div>
+                </div>
+                <div class="window-body">
+                    <p>There's so much room for activities!</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style lang="css" scoped>
+.container {
+    max-width: 900px;
+    margin: auto;
+    height: 100%;
+}
+</style>
