b476cb1f8a21d841d9d709557b4c7cfe7ff72ad8
zanebennion <zanebennion@gmail.com>
2026-05-09 13:59:40 -0600

    installed howlerjs

--- app/pages/music.vue
+++ app/pages/music.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="background">
-        
+        <div class="player"></div>
+        <div class="songs"></div>
     </div>
 </template>
+
+<style lang="css" scoped>
+.background {
+    display: flex;
+}
+</style>
