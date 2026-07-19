a2bb469db4e7631e784f437068cc86d26ac8925b
zanebennion <zanebennion@zben.me>
2026-07-18 19:56:36 -0800

    fixed hospicefind background

--- app/pages/hospicefind.vue
+++ app/pages/hospicefind.vue
@@ -56,7 +56,8 @@
 .container {
     width: 100vw;
     height: 100vh;
-    background-image: url("/img/vivintBG.webp");
+    background-image: url("/img/hospicefindBG.webp");
+    background-size: 20% auto;
 }
 
 .window {
